1e903a36d51ce6fd70c6fd1d3d143db253bd9bb5
zhuy-nguyenz <graio911@gmail.com>
2021-09-17 11:01:35 +0700

    1st

---
+++ dist/main.js
@@ -0,0 +1,20 @@
+let money = 10000;
+const buyCar = (car) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve(`can buy ${car}`);
+            }
+            else {
+                reject(`not enought moneh`);
+            }
+        }, 100);
+    }));
+};
+money = 100001;
+const promise = buyCar("vinslow").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+});
+//# sourceMappingURL=main.js.map
